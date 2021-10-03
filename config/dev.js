95d34de8d00cb0d518270628e71d39a419e4a1ad
Guilherme <guilherme.reis@asksuite.com>
2021-10-03 17:35:15 -0300

    first commit

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,9 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://development:nuqG8wsVebnm8BZ@clusterrr-shard-00-00.rlnld.mongodb.net:27017,clusterrr-shard-00-01.rlnld.mongodb.net:27017,clusterrr-shard-00-02.rlnld.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-gjgznb-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
+
+
+//nuqG8wsVebnm8BZ
